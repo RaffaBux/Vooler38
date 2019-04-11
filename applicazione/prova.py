970ce67297251a2e6f1f0831d233bfbaea420597
RaffaBux <raffaelebussolotto@gmail.com>
2019-04-11 10:52:27 +0200

    sistemato sender e iniziato funzioni della finestra di settaggio temeratura

---
+++ applicazione/prova.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'temperatureGUI.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_TempGUI(object):
+    def setupUi(self, TempGUI):
+        TempGUI.setObjectName("TempGUI")
+        TempGUI.resize(464, 387)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/sfondo/2_icon38.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        TempGUI.setWindowIcon(icon)
+        self.temp_grid = QtWidgets.QWidget(TempGUI)
+        self.temp_grid.setObjectName("temp_grid")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.temp_grid)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.monitoraggioButton = QtWidgets.QPushButton(self.temp_grid)
+        self.monitoraggioButton.setObjectName("monitoraggioButton")
+        self.gridLayout_2.addWidget(self.monitoraggioButton, 6, 0, 1, 1)
+        self.tempModLabel = QtWidgets.QLabel(self.temp_grid)
+        self.tempModLabel.setObjectName("tempModLabel")
+        self.gridLayout_2.addWidget(self.tempModLabel, 2, 0, 1, 1)
+        self.statoLabel = QtWidgets.QLabel(self.temp_grid)
+        self.statoLabel.setObjectName("statoLabel")
+        self.gridLayout_2.addWidget(self.statoLabel, 6, 2, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout_2.addItem(spacerItem, 6, 1, 1, 1)
+        self.tempAttLabel2 = QtWidgets.QLabel(self.temp_grid)
+        self.tempAttLabel2.setObjectName("tempAttLabel2")
+        self.gridLayout_2.addWidget(self.tempAttLabel2, 1, 0, 1, 1)
+        self.tempAttLabel1 = QtWidgets.QLabel(self.temp_grid)
+        self.tempAttLabel1.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.tempAttLabel1.setObjectName("tempAttLabel1")
+        self.gridLayout_2.addWidget(self.tempAttLabel1, 1, 1, 1, 1)
+        self.celsiusLabel1 = QtWidgets.QLabel(self.temp_grid)
+        self.celsiusLabel1.setObjectName("celsiusLabel1")
+        self.gridLayout_2.addWidget(self.celsiusLabel1, 1, 2, 1, 1)
+        self.celsiusLabel2 = QtWidgets.QLabel(self.temp_grid)
+        self.celsiusLabel2.setObjectName("celsiusLabel2")
+        self.gridLayout_2.addWidget(self.celsiusLabel2, 2, 2, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout_2.addItem(spacerItem1, 0, 0, 1, 1)
+        self.tempSpin = QtWidgets.QDoubleSpinBox(self.temp_grid)
+        self.tempSpin.setWrapping(False)
+        self.tempSpin.setFrame(True)
+        self.tempSpin.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.tempSpin.setReadOnly(False)
+        self.tempSpin.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.tempSpin.setAccelerated(False)
+        self.tempSpin.setKeyboardTracking(True)
+        self.tempSpin.setPrefix("")
+        self.tempSpin.setDecimals(1)
+        self.tempSpin.setMinimum(5.0)
+        self.tempSpin.setMaximum(25.0)
+        self.tempSpin.setSingleStep(0.1)
+        self.tempSpin.setProperty("value", 16.0)
+        self.tempSpin.setObjectName("tempSpin")
+        self.gridLayout_2.addWidget(self.tempSpin, 2, 1, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.confermaButton = QtWidgets.QPushButton(self.temp_grid)
+        self.confermaButton.setObjectName("confermaButton")
+        self.horizontalLayout.addWidget(self.confermaButton)
+        self.resetButton = QtWidgets.QPushButton(self.temp_grid)
+        self.resetButton.setObjectName("resetButton")
+        self.horizontalLayout.addWidget(self.resetButton)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 3, 1, 2, 1)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout_2.addItem(spacerItem2, 5, 1, 1, 1)
+        TempGUI.setCentralWidget(self.temp_grid)
+
+        self.retranslateUi(TempGUI)
+        QtCore.QMetaObject.connectSlotsByName(TempGUI)
+
+    def retranslateUi(self, TempGUI):
+        _translate = QtCore.QCoreApplication.translate
+        TempGUI.setWindowTitle(_translate("TempGUI", "******"))
+        self.monitoraggioButton.setText(_translate("TempGUI", "Monitoraggio"))
+        self.tempModLabel.setText(_translate("TempGUI", "Temperatura modificabile: "))
+        self.statoLabel.setText(_translate("TempGUI", "<html><head/><body><p><img src=\":/sfondo/gray.png\"/></p></body></html>"))
+        self.tempAttLabel2.setText(_translate("TempGUI", "Temperatura ******:"))
+        self.tempAttLabel1.setText(_translate("TempGUI", "---"))
+        self.celsiusLabel1.setText(_translate("TempGUI", "°C"))
+        self.celsiusLabel2.setText(_translate("TempGUI", "°C"))
+        self.confermaButton.setText(_translate("TempGUI", "Conferma"))
+        self.resetButton.setText(_translate("TempGUI", "Reset"))
+
+import sfondoCantina_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    TempGUI = QtWidgets.QMainWindow()
+    ui = Ui_TempGUI()
+    ui.setupUi(TempGUI)
+    TempGUI.show()
+    sys.exit(app.exec_())
+
--- cantina.sql
+++ cantina.sql
@@ -16,27 +16,104 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Utente`
+-- Table structure for table `Botte`
 --
 
-DROP TABLE IF EXISTS `Utente`;
+DROP TABLE IF EXISTS `Botte`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Utente` (
-  `codUtente` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  PRIMARY KEY (`codUtente`)
+CREATE TABLE `Botte` (
+  `idBotte` int(11) NOT NULL,
+  `contenuto` varchar(20) NOT NULL,
+  `tempBotte` double DEFAULT NULL,
+  `tempsetBotte` double DEFAULT NULL,
+  `idLocale` int(11) NOT NULL,
+  PRIMARY KEY (`idBotte`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Utente`
+-- Dumping data for table `Botte`
 --
 
-LOCK TABLES `Utente` WRITE;
-/*!40000 ALTER TABLE `Utente` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Utente` ENABLE KEYS */;
+LOCK TABLES `Botte` WRITE;
+/*!40000 ALTER TABLE `Botte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Botte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Locale`
+--
+
+DROP TABLE IF EXISTS `Locale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Locale` (
+  `idLocale` int(11) NOT NULL AUTO_INCREMENT,
+  `tempLocale` double DEFAULT NULL,
+  `tempsetLocale` double DEFAULT NULL,
+  PRIMARY KEY (`idLocale`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Locale`
+--
+
+LOCK TABLES `Locale` WRITE;
+/*!40000 ALTER TABLE `Locale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Locale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `StoricoBotte`
+--
+
+DROP TABLE IF EXISTS `StoricoBotte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `StoricoBotte` (
+  `idStoricoBotte` int(11) NOT NULL AUTO_INCREMENT,
+  `dataAggB` date NOT NULL,
+  `contenutoAggB` varchar(20) DEFAULT NULL,
+  `tempAggB` double NOT NULL,
+  `idBotte` int(11) NOT NULL,
+  PRIMARY KEY (`idStoricoBotte`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `StoricoBotte`
+--
+
+LOCK TABLES `StoricoBotte` WRITE;
+/*!40000 ALTER TABLE `StoricoBotte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `StoricoBotte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `StoricoLocale`
+--
+
+DROP TABLE IF EXISTS `StoricoLocale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `StoricoLocale` (
+  `idStoricoLocale` int(11) NOT NULL AUTO_INCREMENT,
+  `dataAggL` date NOT NULL,
+  `tempAggL` double NOT NULL,
+  `idLocale` int(11) NOT NULL,
+  PRIMARY KEY (`idStoricoLocale`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `StoricoLocale`
+--
+
+LOCK TABLES `StoricoLocale` WRITE;
+/*!40000 ALTER TABLE `StoricoLocale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `StoricoLocale` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -48,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-18 11:37:52
+-- Dump completed on 2019-04-11 10:42:47
